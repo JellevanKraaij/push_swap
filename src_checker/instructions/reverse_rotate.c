3063b71d97bfc180ad0033c0701af10b2a7d130a
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-04-12 17:00:24 +0200

    fix: add protection to all reverse rotate functions

--- src_checker/instructions/reverse_rotate.c
+++ src_checker/instructions/reverse_rotate.c
@@ -14,16 +14,22 @@
 
 void	reverse_rotate_a(t_vars *vars)
 {
+	if (lststack_length(vars->stack_a) < 1)
+		return ;
 	vars->stack_a = vars->stack_a->prev;
 }
 
 void	reverse_rotate_b(t_vars *vars)
 {
+	if (lststack_length(vars->stack_b) < 1)
+		return ;
 	vars->stack_b = vars->stack_b->prev;
 }
 
 void	reverse_rotate_ab(t_vars *vars)
 {
+	if (lststack_length(vars->stack_a) < 1 || \
+	lststack_length(vars->stack_b) < 1)
 	vars->stack_a = vars->stack_a->prev;
 	vars->stack_b = vars->stack_b->prev;
 }
